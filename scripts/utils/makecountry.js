99fcfb8bcc2ae1507052eed86891bd17e45d52d1
CreamBread868 <tomio.miyazaki@gmail.com>
2026-01-06 13:08:46 +0900

    uiの方もinit

--- scripts/utils/makecountry.js
+++ scripts/utils/makecountry.js
@@ -1,8 +1,13 @@
 import * as server from "@minecraft/server"
 const { world, system } = server;
+import { ActionFormData, ModalFormData, MessageFormData } from "@minecraft/server-ui"
 const countryDatas = new Dypro("country");
 export class MakeCountry {
 
+    static makeForm(player) {
+        const form = new ActionFormData()
+
+    }
     //constructurはなし
     /**
      * 国を作る
