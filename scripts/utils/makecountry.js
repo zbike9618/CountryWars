e4dd4a179c6f4ce423ea5e25ec78c66fba2b4703
CreamBread868 <tomio.miyazaki@gmail.com>
2026-01-06 19:09:08 +0900

    makeCountryのformを大体完成 国の設定急募

--- scripts/utils/makecountry.js
+++ scripts/utils/makecountry.js
@@ -5,9 +5,14 @@ import { Util } from "./util.js";
 const countryDatas = new Dypro("country");
 export class MakeCountry {
 
-    static makeForm(player) {
+    static async makeForm(player) {
         const form = new ModalFormData()
-        form.header({ translate: "cw.mcform.header" })
+        form.title({ translate: "cw.mcform.title" })
+        form.textField({ translate: "cw.mcform.WriteCountryNameLabel" }, { translate: "cw.mcform.WriteCountryNamePlaceholder" })//国名
+        form.toggle({ translate: "cw.mcform.toggle" }, { defaultValue: false, tooltip: { translate: "cw.mcform.toggleTooltip" } })//平和主義か
+        const res = await form.show(player)
+        if (res.canceled) return;
+        make(player, res.formValues)
 
     }
     //constructurはなし
@@ -16,8 +21,16 @@ export class MakeCountry {
      * @param {import("@minecraft/server").Entity} player 
      * 
      */
-    static make(player) {
+    static make(player, { countryName, isPeace }) {
+        const countryData =
+        {
+            name: countryName,
+            money: 0,
+            isPeace: isPeace,
 
+        }
+        const id = countryDatas.idList.length + 1;
+        countryDatas.set(id, countryData);
     }
 
 }
